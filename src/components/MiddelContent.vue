58ea97f5eb198834ac2c77c0216331d477e90b7f
Sixapcks123 <aub.heurtault@gmail.com>
2022-02-03 16:29:45 +0100

    create middel content

--- src/components/MiddelContent.vue
+++ src/components/MiddelContent.vue
@@ -1,6 +1,8 @@
 <template>
  <div>
-   
+   <div id="element2">
+    <div id="element1">ploum ploum</div>
+</div>
  </div>
 </template>
 <script>
@@ -9,5 +11,22 @@ export default {
 }
 </script>
 <style scoped>
-
+#element1 {
+    position:absolute;
+    background-color:#00CCFF; /* bleu ciel */
+    width:50px;
+    height:50px;
+    text-align:center;
+    top:55px;
+    left:42px;
+    }
+#element2 {
+    position:absolute;
+    background-color:#CC66CC; /* violet */
+    width:100px;
+    height:100px;
+    text-align:center;
+    top:50px;
+    left:70px;
+    }
 </style>
